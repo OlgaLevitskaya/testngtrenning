d7697ee8448ce9cb5ad469b6b68502f7f50741a2
OlgaLevitskaya <levitskaya-olga@yandex.ru>
2018-05-28 23:10:41 +0300

    add assert

--- src/test/java/ru/testng/Task3Test.java
+++ src/test/java/ru/testng/Task3Test.java
@@ -10,27 +10,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-/**
- * Практическое задание №3
- * <p>
- * Разработайте и добавьте в созданный ранее проект несколько тестов (2-3 позитивных теста и 1-2 негативных теста).
- * <p>
- * Тестировать будем функцию создания файла File.createNewFile(), входящую в стандартную библиотеку Java.
- * <p>
- * Не нужно делать сложные тесты, со сложной логикой или сложной архитектурой. Наша цель -- изучение возможностей
- * тестового фреймворка. Поэтому делайте несложные тесты.
- * <p>
- * Можно сделать тесты пока без проверок, если тестируемые операции отработали и ничего не упало -- значит всё хорошо.
- * <p>
- * Если Вы хотите вместо учебного примера сделать тесты для какого-то собственного приложения -- отлично!
- * Но не нарушайте условия неразглашения конфиденциальной информации.
- * Сделайте для этих тестов фикстуру. Она должна готовить временную директорию, в которой и будут выполняться попытки
- * создания файла, а после завершения теста эта временная директория должна быть удалена.
- * (Для создания временной директории можно использовать функцию c)
- * <p>
- * Если Вы делали тесты для своего собственного приложения -- подумайте, какая фикстура могла бы там быть полезна.
- * Убедитесь, что тесты запускаются в консоли и в среде разработки.
- */
 public class Task3Test {
     private Path tmp;
 
@@ -41,42 +20,46 @@ public class Task3Test {
     }
 
     @Test(groups = "positive")
-    public void createFileSuccess1() throws IOException {
-        createFile(tmp.toString(), "temp");
+    public void createFileSuccess1() {
+        Assert.assertTrue(createFile(tmp.toString(), "temp"), "File isn't create!!!");
     }
 
     @Test(groups = "positive")
-    public void createFileSuccess2() throws IOException {
-        createFile(tmp.toString(), "temp.txt");
-    }
-
-    @Test(groups = "negative", expectedExceptions = IOException.class)
-    public void createFileNoDir() throws IOException {
-        createFile(tmp + "123", "temp1");
-    }
-
-    @Test(groups = "negative", expectedExceptions = IOException.class)
-    public void createFileWrongDir() throws IOException {
-        createFile(null, "temp1");
+    public void createFileSuccess2() {
+        Assert.assertTrue(createFile(tmp.toString(), "temp.txt"), "File isn't create!!!");
     }
 
     @Test(groups = "negative")
-    public void createFileNameEmptyName() throws IOException {
-        Assert.assertFalse(createFile(tmp.toString(), ""), "File create!!!");
+    public void createFileNoDir() {
+        Assert.assertFalse(createFile(tmp + "123", "temp1"), "File is create!!!");
     }
 
-    @AfterClass (alwaysRun = true)
+    @Test(groups = "negative")
+    public void createFileWrongDir() {
+        Assert.assertFalse(createFile(null, "temp1"), "File is create!!!");
+    }
+
+    @Test(groups = "negative")
+    public void createFileNameEmptyName() {
+        Assert.assertFalse(createFile(tmp.toString(), ""), "File is create!!!");
+    }
+
+    @AfterClass(alwaysRun = true)
     public void tearDown() {
         if (tmp != null) tmp.toFile().deleteOnExit();
         System.out.println("Delete directory " + tmp);
     }
 
-    private boolean createFile(String path, String name) throws IOException {
+    private boolean createFile(String path, String name) {
         String fullPath = path + "\\" + name;
         File file = new File(fullPath);
-        boolean res = file.createNewFile();
-        if (res) System.out.println("Create file " + fullPath);
-        else System.out.println("Can't create file " + fullPath);
+        boolean res = false;
+        try {
+            res = file.createNewFile();
+            System.out.println("Create file " + fullPath);
+        } catch (IOException e) {
+            System.out.println("Can't create file " + fullPath);
+        }
         return res;
     }
 }
